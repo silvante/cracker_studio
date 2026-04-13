6584dfa437a118302d7dfff32dd384a8877041c6
silvante <khamidov.ko@gmail.com>
2026-04-13 22:24:19 +0500

    Claude might just help with easy stuff

--- src/app/layouts/MainLayout/MainLayout.tsx
+++ src/app/layouts/MainLayout/MainLayout.tsx
@@ -3,9 +3,11 @@ import Header from "./ui/Header";
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className="flex flex-col h-screen">
       <Header />
-      <Outlet />
+      <main className="flex-1 overflow-auto">
+        <Outlet />
+      </main>
     </div>
   );
 };
